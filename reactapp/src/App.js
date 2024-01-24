d17a2f4b36f088b9b896da18a9af73a40a085496
Albarwuah <albarwuah@Albarwuahs-MBP.lan>
2024-01-24 01:52:35 -0500

    react

---
+++ reactapp/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Login from './login'
+import Navi from './nav' 
+
+function App() {
+  return (
+   <div>
+    <Navi />
+    <Login />
+
+</div>
+
+
+   
+  );
+}
+
+export default App;
---
+++ reactapp/src/login.js
@@ -0,0 +1,22 @@
+
+
+import React, {Component} from 'react';
+
+export default class Login extends Component{
+    render(){
+        return (
+            <nav className='LoginForm'>
+
+                <input placeholder="enter Username"></input>
+                <input placeholder="enter Password"></input>
+               
+
+                <h3> Log in</h3>
+            
+
+         
+
+            </nav>
+        )
+    }
+}
